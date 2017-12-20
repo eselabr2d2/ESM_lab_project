60809c209b15a28041747ca32d974411eafaeabf
Daniel Leoncio Paredes Zevallos <dapar002@fb4.fh>
2017-12-20 17:02:43 +0100

    RTOS works

--- ESM_lab_project/ESM_lab_project/src/main.c
+++ ESM_lab_project/ESM_lab_project/src/main.c
@@ -10,13 +10,14 @@ static void blinky(void *pvParameters);
 static void adc_tester(void *pvParameters);
 static void ir_tester(void *pvParameters);
 
+
 int main()
 {
 	dorobo_init();			//Call dorobo_init() function to initialize HAL, Clocks, Timers etc.	
     
-	//xTaskCreate(blinky, "BLINKYTASK", 512, NULL, 2, NULL);	//create microswitches tester task
-//	xTaskCreate(adc_tester, "ADCTASK", 512, NULL, 2, NULL); //  creat adc tester task
-	xTaskCreate(ir_tester, "IRTASK", 512, NULL, 2, NULL); 
+	xTaskCreate(blinky, "BLINKYTASK", 216, NULL, 2, NULL);	//create microswitches tester task
+	xTaskCreate(adc_tester, "ADCTASK", 216, NULL, 1, NULL); //  creat adc tester task
+	//xTaskCreate(ir_tester, "IRTASK", 512, NULL, 2, NULL); 
 
 	vTaskStartScheduler();	//start the freertos scheduler
 
@@ -31,9 +32,9 @@ static void blinky(void *pvParameters) {
 
 	DD_PINLEVEL_T sw01, sw02 ;
 	
-	uint32_t delay = 100;
-	while (1) 
-	{
+	uint32_t delay = 200;
+	while (1) {
+		traces("Task 1");
 		sw01 = digital_get_pin(DD_PIN_PC13);
 		sw02 = digital_get_pin(DD_PIN_PA8);
 		
@@ -53,17 +54,17 @@ static void adc_tester(void *pvParameters) {
 	adc_init();
 
 	uint32_t left, right ;
-	uint32_t delay = 100;
+	uint32_t delay = 200;
 
-	while (1) 
-	{
+	while (1) {
+		traces("Task 2");
 		left 	= adc_get_value(DA_ADC_CHANNEL0);
 		right 	= adc_get_value(DA_ADC_CHANNEL8);
 		
 		tracef("L = %i\n", left);
 		vTaskDelay(delay);				//delay the task for 20 ticks (1 ticks = 50 ms)
-		tracef("R = %i\n", right);
-		vTaskDelay(delay);				//delay the task for 20 ticks (1 ticks = 50 ms)
+	//	tracef("R = %i\n", right);
+	//	vTaskDelay(delay);				//delay the task for 20 ticks (1 ticks = 50 ms)
 	}
 }
 

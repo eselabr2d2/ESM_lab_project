c1cfa57b243a4f90c547342a17e302accb687093
Daniel Leoncio Paredes Zevallos <dapar002@fb4.fh>
2017-12-20 12:27:14 +0100

    led worked

--- ESM_lab_project/ESM_lab_project/src/main.c
+++ ESM_lab_project/ESM_lab_project/src/main.c
@@ -9,8 +9,9 @@ static void blinky(void *pvParameters);
 
 int main()
 {
-    dorobo_init();			//Call dorobo_init() function to initialize HAL, Clocks, Timers etc.	
-	
+	dorobo_init();			//Call dorobo_init() function to initialize HAL, Clocks, Timers etc.	
+
+    
 	xTaskCreate(blinky, "BLINKYTASK", 512, NULL, 2, NULL);	//create blinky task
 
 	vTaskStartScheduler();	//start the freertos scheduler
@@ -18,15 +19,26 @@ int main()
 	return 0;				//should not be reached!
 }
 
-static void blinky(void *pvParameters) 
-{
+static void blinky(void *pvParameters) {
+
+	// microswitch tester
 	trace_init();
-  uint32_t delay = 500;
+	digital_configure_pin( DD_PIN_PC13, DD_CFG_INPUT_PULLUP); 
+	digital_configure_pin( DD_PIN_PA8, DD_CFG_INPUT_PULLUP);	
+
+	DD_PINLEVEL_T sw01, sw02 ;
+	
+	uint32_t delay = 100;
 	while (1) 
 	{
-		led_green_toggle();
-		led_red_toggle();
-		traces("toogle led");		//print debug message
+		sw01 = digital_get_pin(DD_PIN_PC13);
+		sw02 = digital_get_pin(DD_PIN_PA8);
+		
+		if (sw01 == DD_LEVEL_HIGH ) 
+			led_red_toggle();
+		if (sw02 == DD_LEVEL_HIGH)
+			led_green_toggle();
+
 		vTaskDelay(delay);				//delay the task for 20 ticks (1 ticks = 50 ms)
 	}
 }

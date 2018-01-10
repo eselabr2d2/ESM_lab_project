e559897c6b6b90fd5354385f3a8d4fb63d751a1b
Your Name <you@example.com>
2018-01-10 11:44:52 +0100

    Acceleration functions with defined STEP and STEP_DELAY

--- r2d2/include/driver.h
+++ r2d2/include/driver.h
@@ -1,2 +1,13 @@
-void motor_acc(enum DM_MOTORS_E motor[], int8_t aim_speed[], int8_t step[], int8_t size, TickType_t delay);
+/*
+ * test.h
+ *
+ *  Function for driving the motor
+ *      Author: Pedro
+ */
 
+#ifndef INCLUDE_DRIVER_H_
+#define INCLUDE_DRIVER_H_
+
+void accelerator(enum DM_MOTORS_E motor[], int8_t aim_speed[], int8_t size);
+
+#endif /* INCLUDE_DRIVER_H_ */

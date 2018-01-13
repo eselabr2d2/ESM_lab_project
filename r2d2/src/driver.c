80f5a002734eb76a49b603bbd673c64b16b8d92b
daleonpz <daleonpz@gmail.com>
2018-01-13 19:56:02 +0100

    udpate functions

--- r2d2/src/driver.c
+++ r2d2/src/driver.c
@@ -1,35 +1,24 @@
-#include <stdlib.h>
-#include "motor.h"
-#include "FreeRTOS.h"
-#include "task.h"
+#include "driver.h"
 
 // TODO: Check the step and its delay.
 #define STEP 5
 #define STEP_DELAY 10
 
-// TODO: Check the cancel parameter.
-// Function to change the speed of the three motors.
-void move(int8_t aim_speed[], uint8_t cancel)
-{
-  enum DM_MOTORS_E motors[] = { DM_MOTOR0, DM_MOTOR1, DM_MOTOR2 };
-  accelerator(motors, aim_speed,  3, cancel);
-}
-
-
 //Function to change the speed of the motors depending on the size of the array.
-void accelerator(enum DM_MOTORS_E motor[], int8_t aim_speed[], int8_t size, uint8_t cancel)
+void accelerator(enum DM_MOTORS_E motor[], int8_t aim_speed[], int8_t size) 
 {
+ 
   int8_t speed_diff[size];
   int8_t speed[size];
   uint8_t any_diff = 0;
 
-  for(uint8_t i = 0; i < size && !cancel; i++)
+  for(uint8_t i = 0; i < size; i++)
   {
     speed[i] = motor_get_speed(motor[i]);
     speed_diff[i] = aim_speed[i] - motor_get_speed(motor[i]);
     any_diff = any_diff || (speed_diff[i] != 0);
   }
-  while(any_diff && !cancel)
+  while(any_diff)
   {
     any_diff = 0;
     for(uint8_t i = 0; i < size; i++)
@@ -56,5 +45,15 @@ void accelerator(enum DM_MOTORS_E motor[], int8_t aim_speed[], int8_t size, uint
       }
     }
     vTaskDelay(STEP_DELAY);
-  }
+   }
 }
+
+
+// TODO: Check the stopMotor parameter.
+// Function to change the speed of the three motors.
+void move(int8_t aim_speed[]){
+  enum DM_MOTORS_E motors[] = { DM_MOTOR0, DM_MOTOR1, DM_MOTOR2 };
+  accelerator(motors, aim_speed,  3);
+}
+
+

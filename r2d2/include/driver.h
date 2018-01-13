8d35d042d54b8edf51d5550e8f2c085df9eb7572
daleonpz <daleonpz@gmail.com>
2018-01-13 19:52:42 +0100

    modified headers

--- r2d2/include/driver.h
+++ r2d2/include/driver.h
@@ -1,15 +1,20 @@
 /*
- * test.h
+ * driver.h
  *
  *  Functions for driving the motor
  *      Authors: Group 10
  */
 
-#ifndef INCLUDE_DRIVER_H_
-#define INCLUDE_DRIVER_H_
+#ifndef _DRIVER_H_
+#define _DRIVER_H_
 
-void move(int8_t aim_speed[], uint8_t cancel);
+#include <stdlib.h>
+#include <stdint.h>
+#include "FreeRTOS.h"
+#include "motor.h"
+#include "task.h"
 
-void accelerator(enum DM_MOTORS_E motor[], int8_t aim_speed[], int8_t size, uint8_t  cancel);
+void accelerator(enum DM_MOTORS_E motor[], int8_t aim_speed[], int8_t size);
+void move(int8_t aim_speed[]);
 
 #endif /* INCLUDE_DRIVER_H_ */
--- r2d2/include/search.h
+++ r2d2/include/search.h
@@ -4,9 +4,14 @@
  *   Functions for searching the tower
  *      Authors: Group 10
  */
+#ifndef _SEARCH_H_
+#define _SEARCH_H_
 
-#ifndef INCLUDE_SEARCH_H_
-#define INCLUDE_SEARCH_H_
+#include "dorobo32.h"
+#include "trace.h"
+#include "adc.h"
+#include "fft.h"
+#include "driver.h"
 
 void search();
 
